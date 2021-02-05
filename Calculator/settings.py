94de120903bd98ca1d108d2af8276f34c03c636f
Ketan Patil <68143477+ketanpatil-bvp@users.noreply.github.com>
2021-02-05 20:38:55 +0530

    Whitenoise 5-FEB-2021

--- Calculator/settings.py
+++ Calculator/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'compression_middleware.middleware.CompressionMiddleware',
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
