5a9cdb22ad432bdddd0cd3d6240bf5058af382d8
Ketan Patil <68143477+ketanpatil-bvp@users.noreply.github.com>
2021-02-23 17:18:19 +0530

    Second Commit

--- Calculator/settings.py
+++ Calculator/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY','%^h33&ue$@=*eys716-(iw6@otu(%%a7eu@dyf26&*n
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'mathtools.herokuapp.com','127.0.0.1'
